0a3152d824ef3d76a12f4557712ac9e2f2151976
anton svedin <anton.svedin@elev.ga.ntig.se>
2026-01-19 11:05:28 +0100

    Börjat lite med menyn

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
+    ArrayList<String> bestallnig = new ArrayList<String>();
+    static boolean On = true;
+    static Scanner tangentbord = new Scanner(System.in);
+    static int val;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        while (On) {
+        System.out.println("1. Lägg en beställning");
+        System.out.println("2. Se beställningar");
+        System.out.println("3. Priser");
+        System.out.println("4. Avsluta");
+
+        switch (val) {
+            case 1:
+                
+                break;
+            
+            case 2:
+                
+                break;
+
+            case 3:
+                
+                break;  
+        }
+        }
     }
 }
